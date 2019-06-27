1978e5c45c074062e6244730ebc54c42594cc51f
N04h <contact@n04h.jp>
2019-06-27 19:56:39 +0900

    update ruby/basic3 真偽値を返すメソッド

--- ruby/basic3.rb
+++ ruby/basic3.rb
@@ -2,9 +2,16 @@
 # upcase  : 文字列を大文字にしたものを返すだけ
 # upcase! : 文字列を大文字にしたものを返しつつ、もとの文字列も大文字にする
 
-name = "tarou"
+name = 'tarou'
 puts name.upcase
 puts name
 
 puts name.upcase!
 puts name
+
+# ? - 真偽値を返すメソッド
+# empty?   : 文字列オブジェクトが空かどうか
+# include? : 文字列に特定の文字が含まれているかどうか
+
+p name.empty? # false
+p name.include?('A') # true
